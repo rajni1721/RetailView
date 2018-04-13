049e3dfc255e833c929e15e913744db0adcf4946
Amjad Khan <khan.amjad36@gmail.com>
2018-04-13 14:00:57 +0530

    Initial Commit

---
+++ RetailView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RetailView
+//
+//  Created by Babu on 4/13/18.
+//  Copyright © 2018 Babu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
